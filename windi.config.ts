00b41b670843ef651e6c168424f7c41640cde356
lnxcz <frnoch@pm.me>
2022-11-03 15:23:14 +0100

    feat: :sparkles: add windicss

---
+++ windi.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "windicss/helpers";
+import formsPlugin from "windicss/plugin/forms";
+
+export default defineConfig({
+  darkMode: "class",
+  // safelist: "p-3 p-4 p-5",
+  // theme: {},
+  plugins: [formsPlugin],
+});
