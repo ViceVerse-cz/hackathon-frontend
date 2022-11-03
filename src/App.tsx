8e7151aedf7caa54fae917b5b96b1aa47d40a875
lnxcz <frnoch@pm.me>
2022-11-03 15:34:31 +0100

    feat: :sparkles: default page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,18 @@ import reactLogo from "./assets/react.svg";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="text-blue-400">ahoj kamarade</div>;
+  return (
+    <div className="text-gray-900 p-5">
+      <p>milujeme hackathon</p>
+      <button
+        className="bg-blue-300 rounded text-white"
+        onClick={() => setCount(count + 1)}
+      >
+        klik klik
+      </button>
+      <p className="text-red-500">kliknuto: {count}</p>
+    </div>
+  );
 }
 
 export default App;
