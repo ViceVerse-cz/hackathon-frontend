3fb66f93100c0eebefea76b2f779fd8187b0e758
Atsukoro1 <dornicakkuba@gmail.com>
2022-11-04 22:09:06 +0100

    feat: 🎸 Register and login UI

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -9,6 +9,9 @@ export default function SignIn() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const inputStyle = `bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 bg-gray-700
+    border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 w-full`;
+
     function handleSubmit(e: FormEvent) {
         e.preventDefault();
         if (email === '' || password === '') {
@@ -19,31 +22,57 @@ export default function SignIn() {
     }
 
     return (
-        <div id="SingIn">
-            <form className="content" onSubmit={handleSubmit}>
-                <h1>Přihlášení</h1>
+        <div className='bg-slate-700 w-full h-[100vh]' id="SingIn">
+            <span>&nbsp;</span>
 
-                <div className="input-group">
-                    <p>Email</p>
-                    <input
-                        type="email"
-                        onChange={(e) => setEmail(e.target.value)}
+            <form className="content w-[500px] h-fit mt-52 ml-auto mr-auto bg-[#1e293b] p-5 rounded-lg" onSubmit={handleSubmit}>
+                <h1 className='text-3xl text-white'>Přihlášení</h1>
+
+                <div>
+                    <label className="block mb-2 text-sm mt-3 font-medium text-white">Email</label>
+                    <input 
+                        type="email" 
+                        id="email" 
+                        name='email'
                         value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        className={inputStyle} 
+                        placeholder="example@gmail.com" 
+                        required
                     />
                 </div>
 
-                <div className="input-group">
-                    <p>Heslo</p>
-                    <input
-                        type="password"
-                        onChange={(e) => setPassword(e.target.value)}
+                <div>
+                    <label className="block mb-2 mt-3 text-sm font-medium text-white">Password</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name='password'
+                        className={inputStyle} 
                         value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        placeholder="********" 
+                        required
                     />
                 </div>
 
-                <Link to="/signup">Vytvořit účet.</Link>
+                <button 
+                    type="submit"
+                    className='w-full mt-8 py-2 text-sm font-medium text-white bg-indigo-500 rounded-lg hover:bg-blue-600'
+                >
+                    Přihlásit se
+                </button>
 
-                <button type="submit">Přihlásit se</button>
+                <div className='w-fit ml-auto mr-auto mt-5'>
+                    <div className='text-center'> 
+                        <Link 
+                            to="/signup"
+                            className='text-indigo-500 mt-2'
+                        >
+                            Vytvořit účet.
+                        </Link>
+                    </div>
+                </div>
             </form>
         </div>
     );
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -17,6 +17,9 @@ export default function SingUp() {
         history('/');
     }
 
+    const inputStyle = `bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 bg-gray-700
+    border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500`;
+
     async function handleSubmit(e: FormEvent) {
         e.preventDefault();
 
@@ -36,53 +39,83 @@ export default function SingUp() {
     }
 
     return (
-        <div id="SingUp">
-            <form className="content" onSubmit={handleSubmit}>
+        <div className='bg-slate-700 w-full h-[100vh]' id="SingUp">
+            <span>&nbsp;</span>
+
+            <form className="content w-[500px] h-fit mt-52 ml-auto mr-auto bg-[#1e293b] p-5 rounded-lg" onSubmit={handleSubmit}>
                 <ArrowLeft
                     className="icon"
                     size={24}
-                    color="#15b6d6"
+                    color="#6366f1"
                     onClick={handleNavigateBack}
                 />
 
-                <h1>Přihlášení</h1>
+                <h1 className='text-3xl text-white'>Create a new account</h1>
 
-                <div className="input-group">
-                    <p>Jméno</p>
-                    <input
-                        type="text"
-                        onChange={(e) => setName(e.target.value)}
-                        value={name}
-                    />
-                </div>
-                <div className="input-group">
-                    <p>Příjmení</p>
-                    <input
-                        type="text"
-                        onChange={(e) => setSurname(e.target.value)}
-                        value={surname}
-                    />
+                <div className='inline-flex mb-3 mt-3'>
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-white">First name</label>
+                        <input 
+                            type="text" 
+                            id="name" 
+                            name='name'
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                            className={inputStyle} 
+                            placeholder="John" 
+                            required
+                        />
+                    </div>
+
+                    <div className='ml-6'>
+                        <label className="block mb-2 text-sm font-medium text-white">Surname</label>
+                        <input 
+                            type="text" 
+                            id="surname" 
+                            name='surname'
+                            value={surname}
+                            onChange={e => setSurname(e.target.value)}
+                            className={inputStyle} 
+                            placeholder="Asshole2" 
+                            required
+                        />
+                    </div>
                 </div>
 
-                <div className="input-group">
-                    <p>Email</p>
-                    <input
-                        type="email"
-                        onChange={(e) => setEmail(e.target.value)}
+                <div>
+                    <label className="block mb-2 text-sm mt-3 font-medium text-white">Email</label>
+                    <input 
+                        type="email" 
+                        id="email" 
+                        name='email'
                         value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        className={inputStyle} 
+                        placeholder="example@gmail.com" 
+                        required
                     />
                 </div>
 
-                <div className="input-group">
-                    <p>Heslo</p>
-                    <input
-                        type="password"
-                        onChange={(e) => setPassword(e.target.value)}
+                <div>
+                    <label className="block mb-2 mt-3 text-sm font-medium text-white">Password</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name='password'
+                        className={inputStyle} 
                         value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        placeholder="********" 
+                        required
                     />
                 </div>
 
-                <button type="submit">Registrovat se</button>
+                <button 
+                    type="submit"
+                    className='w-full mt-8 py-2 text-sm font-medium text-white bg-indigo-500 rounded-lg hover:bg-blue-600'
+                >
+                    Registrovat se
+                </button>
             </form>
         </div>
     );
