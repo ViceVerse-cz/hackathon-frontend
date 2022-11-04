3c59eeb23deb56e0398857798be34ecfe28ff369
Atsukoro1 <dornicakkuba@gmail.com>
2022-11-04 20:48:05 +0100

    feat: 🎸 Building

--- src/pages/Dashboard/components/BuildingLayout/Menu/index.tsx
+++ src/pages/Dashboard/components/BuildingLayout/Menu/index.tsx
@@ -1,7 +1,10 @@
 import { Building } from "../../../../../interfaces/building.interface";
+import Interactive from "./Interactive";
 
 export default function Menu({ building }: { building: Building }) {
     return (
-        <div className="w-auto h-[70%] bg-black rounded text-white">Druhy menu</div>
+        <div className="w-auto h-[70%] bg-black rounded text-white">
+            <Interactive/>
+        </div>
     )
 }
