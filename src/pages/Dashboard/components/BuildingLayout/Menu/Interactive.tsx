73bfbbfa726aa10155a532397e917cffa4cbebd8
lnxcz <david.frnoch@creativehill.cz>
2022-11-04 21:51:31 +0100

    fix: :bug: remove unnecessary @ts-ignore

--- src/pages/Dashboard/components/BuildingLayout/Menu/Interactive.tsx
+++ src/pages/Dashboard/components/BuildingLayout/Menu/Interactive.tsx
@@ -1,102 +1,112 @@
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas, ThreeElements, useFrame } from '@react-three/fiber';
 import { useEffect, useRef, useState } from 'react';
 
 interface IBoxProps {
-  position: [number, number, number],
-  active: boolean,
-  onSelected: () => void
+    position: [number, number, number],
+    active: boolean,
+    onSelected: () => void
+}
+
+interface Mesh {
+    position: [number, number, number],
+    rotation: {
+        x: number,
+        y: number,
+        z: number
+    }
+    scale: [number, number, number],
+    material: any,
+    geometry: any
 }
 
 function Box(props: IBoxProps) {
-  const mesh = useRef()
+    const mesh = useRef() as React.MutableRefObject<Mesh>;
 
-  const [active, setActive] = useState(props.active)
+    const [active, setActive] = useState(props.active)
 
-  useEffect(() => {
-    setActive(props.active)
-  }, [props]);
+    useEffect(() => {
+        setActive(props.active)
+    }, [props]);
 
-  // Animate box on every frame
-  useFrame((state, _) => (
-    // @ts-ignore
-    mesh.current.rotation.y += 0.003
-  ));
+    // Animate box on every frame
+    useFrame((state, _) => (
+        mesh.current.rotation.y += 0.003
+    ));
 
-  // Set background color on every frame
-  useFrame((state, _) => (
-    state.gl.setClearColor("#1e293b")
-  ));
-  
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={2}
-      onClick={props.onSelected}
-    >
-      <boxGeometry args={[1, 0.45, 1]} />
+    // Set background color on every frame
+    useFrame((state, _) => (
+        state.gl.setClearColor("#1e293b")
+    ));
 
-      <meshStandardMaterial color={
-        !active 
-          ? '#4b5563'
-          : '#8B5CF6'
-      } />
-    </mesh>
-  )
+    return (
+        <mesh
+            {...props}
+            ref={mesh}
+            scale={2}
+            onClick={props.onSelected}
+        >
+            <boxGeometry args={[1, 0.45, 1]} />
+
+            <meshStandardMaterial color={
+                !active
+                    ? '#4b5563'
+                    : '#8B5CF6'
+            } />
+        </mesh>
+    )
 }
 
 interface ISeparatorProps {
-  position: [number, number, number],
+    position: [number, number, number],
 }
 
 function Separator(props: ISeparatorProps) {
-  const mesh = useRef();
+    const mesh = useRef() as React.MutableRefObject<Mesh>;
 
-  // Animate box on every frame
-  useFrame((state, _) => (
-    // @ts-ignore
-    mesh.current.rotation.y += 0.003
-  ));
+    // Animate box on every frame
+    useFrame((state, _) => (
+        mesh.current.rotation.y += 0.003
+    ));
 
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={2}
-    >
-      <boxGeometry args={[1, 0.06, 1]} />
-      <meshStandardMaterial color={'#64748b'}/>
-    </mesh>
-  )
+    return (
+        <mesh
+            {...props}
+            ref={mesh}
+            scale={2}
+        >
+            <boxGeometry args={[1, 0.06, 1]} />
+            <meshStandardMaterial color={'#64748b'} />
+        </mesh>
+    )
 }
 
 export default () => {
-  const [active, setActive] = useState<Number>(0);
+    const [active, setActive] = useState<Number>(0);
 
-  return (
-    <Canvas>
-      <pointLight position={[0, 0, 5]} />
+    return (
+        <Canvas>
+            <pointLight position={[0, 0, 5]} />
 
-      {
-        [...Array(1)].map((_, i) => (
-          <>
-            <Box 
-              key={i} 
-              active={active == i} 
-              position={[0, i - 2.51, 0]}
-              onSelected={() => setActive(i)}
-            />
-            <Separator position={[0, i - 2, 0]}/>
-          </>
-        ))
-      }
+            {
+                [...Array(1)].map((_, i) => (
+                    <>
+                        <Box
+                            key={i}
+                            active={active == i}
+                            position={[0, i - 2.51, 0]}
+                            onSelected={() => setActive(i)}
+                        />
+                        <Separator position={[0, i - 2, 0]} />
+                    </>
+                ))
+            }
 
-      <mesh position={[0, -2.56, 0]}
-        scale={2}
-      >
-        <boxGeometry args={[5.5, 0.1, 2]} />
-        <meshStandardMaterial color={"#1e293b"} />
-      </mesh>
-    </Canvas>
-  )
+            <mesh position={[0, -2.56, 0]}
+                scale={2}
+            >
+                <boxGeometry args={[5.5, 0.1, 2]} />
+                <meshStandardMaterial color={"#1e293b"} />
+            </mesh>
+        </Canvas>
+    )
 }
